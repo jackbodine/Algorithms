d9d8e3f119968a21379a3fae0ddd4f7c078543a8
Jack Bodine <JackTaylorBodine@gmail.com>
2024-04-10 15:21:48 +0200

    Modified offline evaluation algorithms to include a variable K for the number of arms.

--- online_and_reinforcement_learning/exp3_offline_evaluation.py
+++ online_and_reinforcement_learning/exp3_offline_evaluation.py
@@ -8,7 +8,8 @@ def learning_rate(t):
 
 
 class EXP3:
-    def __init__(self):
+    def __init__(self, K):
+        self.K = K
         self.L_tilda = [[0 for _ in range(K)]]
         self.t = 0
         self.alg_loss_total = 0
@@ -17,25 +18,29 @@ class EXP3:
 
     def step(self, logged_choice, reward):
         self.t += 1
-        lr = learning_rate(self.t)
+        lr = learning_rate(self.t, self.K)
 
         # Calc Distribution
-        min_double_a = np.min([self.L_tilda[-1][a_double] for a_double in range(K)])
+        min_double_a = np.min(
+            [self.L_tilda[-1][a_double] for a_double in range(self.K)]
+        )
         denominator = np.sum(
-            [np.exp(-lr * (self.L_tilda[-1][a] - min_double_a)) for a in range(K)]
+            [np.exp(-lr * (self.L_tilda[-1][a] - min_double_a)) for a in range(self.K)]
         )
         p_not_logging = [
             np.exp(-lr * (self.L_tilda[-1][a] - min_double_a)) / denominator
-            for a in range(K)
+            for a in range(self.K)
         ]
 
-        A_t = np.random.choice(range(K), p=p_not_logging)
-        l = (1 - reward) * K
+        A_t = np.random.choice(range(self.K), p=p_not_logging)
+        l = (1 - reward) * self.K
 
-        loss_tilda = [0 for _ in range(K)]
+        loss_tilda = [0 for _ in range(self.K)]
         loss_tilda[A_t] = (l) * (logged_choice == A_t) / p_not_logging[A_t]
 
-        self.L_tilda.append([self.L_tilda[-1][a] + loss_tilda[a] for a in range(K)])
+        self.L_tilda.append(
+            [self.L_tilda[-1][a] + loss_tilda[a] for a in range(self.K)]
+        )
 
         # Regret Calculations
         self.alg_loss_total += l * (logged_choice == A_t)
